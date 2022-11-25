9c2b2602d8f6c334dda03a8207b96e7905964e6e
Josemaria Naranjo <josemnaranjoc@gmail.com>
2022-11-25 15:08:19 -0300

    Diseño básico de nav

--- Q-VES/client/src/App.js
+++ Q-VES/client/src/App.js
@@ -7,21 +7,65 @@ import ResultadoFinal from './views/ResultadoFinal.jsx';
 import Comentarios from './views/Comentarios';
 import Register from './views/Register';
 import Login from './views/Login';
+import {UserProvider} from './contexts/userContext';
+
 
 
 
 function App() {
   return (
     <div className="App">
+      <UserProvider>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+          <a class="navbar-brand" href="/">
+            Q-VEMOS
+          </a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item active">
+                <a class="nav-link" href="/">
+                  Home
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  Nuevo juego
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="/register">
+                  Register
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="/login">
+                  Login
+                </a>
+              </li>
+            </ul>
+          </div>
+        </nav>
         <Routes>
-            <Route path='/' element={<Home/>}/>
-            <Route path='/inicio-del-juego' element={<InicioDeJuego/>}/>
-            <Route path='/votaciones' element={<Votaciones/>}/>
-            <Route path='/final-del-juego' element={<ResultadoFinal/>}/>
-            <Route path='/comentarios' element={<Comentarios/>}/>
-            <Route path='/register' element={<Register/>}/>
-            <Route path='/login' element={<Login/>}/>
+          <Route path="/" element={<Home />} />
+          <Route path="/inicio-del-juego" element={<InicioDeJuego />} />
+          <Route path="/votaciones" element={<Votaciones />} />
+          <Route path="/final-del-juego" element={<ResultadoFinal />} />
+          <Route path="/comentarios" element={<Comentarios />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
+      </UserProvider>
     </div>
   );
 }
